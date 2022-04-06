38d7db83e38e0bae70092ae41e587c92470baac0
Sacha SZ <sacha.szendrovics@gmail.com>
2022-04-06 12:10:46 +0200

    "V1.6 du TP - Problème résolu : Toutes les fonctions sont bonnes, interface avancée"

--- main.c
+++ main.c
@@ -62,107 +62,103 @@ int main() {
     fusionlistes(liste_gauche, *liste_droite);
     afficheliste(*liste_gauche);
 
+    int rep = -1;
+    printf("Bienvenu dans la gestion d'une election.\nVoici le menu:\n");
+    while (rep != 10) {
+        affiche_menu();
 
+        printf("Veuillez saisir votre choix :");
+        scanf("%d", &rep);
+        printf("\n");
+        if (rep != 10) {
+            switch (rep) {
+                case 1 :
+//                    printf("Vous avez choisi d'entrer les electeurs.\n");
+//                    char *nom_entrer;
+//                    printf("Veuillez saisir le nom de l'electeur :\n");
+//                    scanf("%s", &nom_entrer);
+//
+//                    long cin_entrer = 0;
+//                    printf("Veuillez entrer le cin de l'electeur :\n");
+//                    scanf("%ld", &cin_entrer);
+//
+//                    int vote_entrer = 0;
+//                    printf("Veuillez saisir le choix de l'electeur :\n");
+//                    scanf("%d", &vote_entrer);
+//
+//                    ajoutelecteur(liste, nom_entrer, cin_entrer, vote_entrer);
+//                    break;
+                    
+                case 2:
+                    if (rep==1) printf("Vous avez choisi d'entrer les electeurs.\n");
+                    else printf("Vous avez choisi d'ajouter les electeurs.\n");
+                    char *nom;
+                    printf("Veuillez saisir le nom de l'electeur :\n");
+                    scanf("%s", &nom);
+
+                    long cin = 0;
+                    printf("Veuillez entrer le cin de l'electeur :\n");
+                    scanf("%ld", &cin);
+
+                    int vote = 0;
+                    printf("Veuillez saisir le choix de l'electeur :\n");
+                    scanf("%d", &vote);
+
+                    ajoutelecteur(liste, nom, cin, vote);
+                    break;
+
+                 case 3 :
+                    printf("Vous avez choisi de supprimer un electeur.\n");
+                    long cin_electeur_supprimercsupprimer_electeur_cin();
+                    Supprimelecteur(liste, cin_electeur_supprimer);
+                    break;
+
+                 case 4 :
+                    printf("Vous avez choisi de rechercher un electeur.\n");
+                    break;
+                 case 5 :
+                    printf("Vous avez d'afficher la liste des electeurs.\n");
+                    break;
+                 case 6 :
+                    printf("Vous avez choisi de calculer le nombre des electeurs.\n");
+                    break;
+                 case 7 :
+                    printf("Vous avez choisi de decouper la liste en trois sous-listes selon les choix: droite, gauche et blanc.\n");
+                    break;
+                 case 8 :
+                    printf("Vous avez choisi de calculer les pourcentages de gauche et de droite pour le 2ème tour.\n");
+                    break;
+                 case 9 :
+                    printf("Vous avez choisi de liberer les listes.\n");
+                    break;
+                default:
+                    printf("Vous avez fait une erreur dans votre choix.\n");
+                    break;
+            }
+        }
+    }
+    printf("Vous avez quitte le programme d'election.\n");
     return 0;
-
 }
 
+long supprimer_electeur_cin(void) {
+    long cin;
+    printf("Vous avez choisi de supprimer un electeur veuillez saisir le cin de l'electeur : ");
+    scanf("%ld", &cin);
+    return cin;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    int rep = -1;
-//    printf("Bienvenu dans la gestion d'une election.\nVoici le menu:\n");
-//    while (rep != 10) {
-//        affiche_menu();
-//
-//        printf("Veuillez saisir votre choix :");
-//        scanf("%d", &rep);
-//        printf("\n");
-//        if (rep != 10) {
-//              if (rep == 1) {
-//                  printf("Vous avez choisi d'entrer les electeurs.\n");
-//                  char *nom;
-//                  printf("Veuillez saisir le nom de l'electeur :\n");
-//                  scanf("%s", &nom);
-//
-//                  long cin = 0;
-//                  printf("Veuillez entrer le cin de l'electeur :\n");
-//                  scanf("%ld", &cin);
-//
-//                  int vote = 0;
-//                  printf("Veuillez saisir le choix de l'electeur :\n");
-//                  scanf("%d", &vote);
-//
-//                  ajoutelecteur(liste, nom, cin, vote);
-//              }
-//              else if (rep == 3) {
-//                  printf("Vous avez choisi de supprimer unn electeur.\n");
-//                  long cin_electeur_supprimer = supprimer_electeur_cin();
-//                  Supprimelecteur(liste, cin_electeur_supprimer);
-//              }
-//              else if (rep==4) {
-//                  printf("Vous avez choisi de rechercher un electeur.\n");
-//              }
-//              else if (rep==5) {
-//                  printf("Vous avez choisi de rechercher un electeur.\n");
-//              }
-//              else if (rep==6) {
-//                  printf("Vous avez choisi de calculer le nombre des electeurs.\n");
-//              }
-//              else if (rep==7) {
-//                  printf("Vous avez choisi de decouper la liste en trois sous-listes selon les choix: droite, gauche et blanc.\n");
-//              }
-//              else if (rep==8) {
-//                  printf("Vous avez choisi de calculer les pourcentages de gauche et de droite pour le 2ème tour.\n");
-//              }
-//              else if (rep==9) {
-//                  printf("Vous avez choisi de liberer les listes.\n");
-//
-//              }
-//        }
-//    }
-//    printf("Vous avez quitte le programme d'election.\n");
-//    return 0;
-//}
-//
-//long supprimer_electeur_cin(void) {
-//    long cin;
-//    printf("Vous avez choisi de supprimer un electeur veuillez saisir le cin de l'electeur : ");
-//    scanf("%ld", &cin);
-//    return cin;
-//}
-//
-//void affiche_menu(void) {
-//    printf("1.\tEntrer les electeurs,\n");
-//    printf("3.\tSupprimer le dernier electeur,\n");
-//    printf("4\tRechercher un electeur,\n");
-//    printf("5\tAfficher la liste des electeurs,\n");
-//    printf("6\tCalculer le nombre des electeurs,\n");
-//    printf("7\tDecouper la liste en trois sous-listes selon les choix: droite, gauche et blanc\n");
-//    printf("\ta\tTrier les sous-listes\n");
-//    printf("\tb\tAfficher les sous-listes,\n");
-//    printf("\tc\tFusionner les deux sous-listes : gauche et droite\n");
-//    printf("8\tCalculer les pourcentages de gauche et de droite pour le 2eme tour,\n");
-//    printf("9\tLiberer les listes\n");
-//    printf("10\tQuitter\n\n");
-//}
+void affiche_menu(void) {
+    printf("1.\tEntrer les electeurs,\n");
+    printf("3.\tSupprimer le dernier electeur,\n");
+    printf("4\tRechercher un electeur,\n");
+    printf("5\tAfficher la liste des electeurs,\n");
+    printf("6\tCalculer le nombre des electeurs,\n");
+    printf("7\tDecouper la liste en trois sous-listes selon les choix: droite, gauche et blanc\n");
+    printf("\ta\tTrier les sous-listes\n");
+    printf("\tb\tAfficher les sous-listes,\n");
+    printf("\tc\tFusionner les deux sous-listes : gauche et droite\n");
+    printf("8\tCalculer les pourcentages de gauche et de droite pour le 2eme tour,\n");
+    printf("9\tLiberer les listes\n");
+    printf("10\tQuitter\n\n");
+}
